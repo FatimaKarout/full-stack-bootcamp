1eb39efaff9bd0f7afd5cda243679c634dd683a1
unknown <fatimakarout1717@gmil.com>
2023-08-21 12:17:45 +0300

    chall

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -1,1 +1,77 @@
-console.log('I am linked now!');
+var name = "fatima";
+alert(name);
+var age = 26;
+let lastName = "kt";
+const DOB = "1997-01-01";
+console.log(age);
+console.log(lastName );
+console.log(DOB);  
+
+
+console.log(typeof"This is a text");
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof 1215);
+console.log(typeof "999")
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+var myText=" string of variable";
+let var1=" double quotation";
+let var2 ='single quotation';
+let var3= `backticks`;
+const counter = 3;
+const backtick = `This is my text using backticks ${counter} times`;
+const singleQuote ='This is my text using single quotations ${counter} times';
+const doubleQuote ='This is my text using double quotations ${counter} times';
+const tryThisAlso = `This is my new version, it’s the ${2 + 3}th version`;
+console.log(myText);
+console.log(var1);
+console.log(var2);
+console.log(var3);
+console.log(counter);
+console.log(backtick);
+console.log(singleQuote);
+console.log(doubleQuote)
+console.log(tryThisAlso);
+console.log("Single and double quotes are used for regular strings.Backticks offer string interpolation, multi-line strings, and expression evaluation");
+
+
+
+
+
+let let1=" first";
+let let2=" second";
+let result=let1+let2;
+console.log(result);
+
+let r1 = (2 === 8);
+console.log(r1);
+let r2 = (2 === '2');
+console.log(r2);
+let r3 = (7 !== 8);
+console.log(r3);
+let r4 = (2 < 8);
+console.log(r4);
+let r5 = (2 <= 1);
+console.log(r5);
+let r6 = (2 > 8);
+console.log(r6);
+let r7= (2 >= 8);
+console.log(r7);
+
+let result0=5+3*2;
+console.log(result0);
+let result1=10/2+3;
+console.log(result1);
+let result2=10/(2+3);
+console.log(result2); 
+console.log("the result are base on the priority of the operation first () than * or / and finally + or -");
+
+
+
+
+
---
+++ JavaScript Challenges/Challenge 2/script.js
@@ -0,0 +1,50 @@
+
+var myage=19;
+ if(myage>=19)
+    console.log("You can enter this room.");
+else console.log("You can't  enter this room.");
+
+const expression1 = 5 > 3;
+const expression2 = 10 === '10';
+const expression3 = true && false;
+const expression4 = !expression3;
+
+console.log(expression1);
+console.log(expression2);
+console.log(expression3);
+console.log(expression4); 
+
+
+
+const age = 25;
+const hasDriverLicense = true;
+const hasCar = false;
+
+let canDrive=false;
+ if(age>=18 && hasDriverLicense==true )
+    canDrive=true;
+console.log(canDrive);
+var canRentCar=false;
+if(age>=22 && hasDriverLicense==true && hasCar==false)
+    canRentCar=true;
+console.log(canRentCar);
+var needDriver=false;
+if(age<18 && hasDriverLicense==false && hasCar==false)
+needDriver=true;
+console.log(needDriver);
+
+
+var role ="admin";
+switch(role) {
+    case "admin":
+     console.log("You have read and write access for the data");
+      break;
+    case "user":
+        console.log("You have only read access for the data");
+      break;
+    case "guest":
+        console.log("You have no access for the data");
+        break;
+    default:
+      console.log("User role is not recoginized");
+  }
